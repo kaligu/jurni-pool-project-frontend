4b480f2d9344e6e27b661bdf86756702c0aae809
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2024-01-17 19:17:29 +0530

    initialized tailwind and material tailwind

---
+++ jurni-pool/src/view/login/LoginView.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoginView() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default LoginView
--- jurni-pool/tailwind.config.js
+++ jurni-pool/tailwind.config.js
@@ -4,12 +4,13 @@
  *   All rights reserved.
  */
 /** @type {import('tailwindcss').Config} */
+// vite.config.js
+import withMT from "@material-tailwind/react/utils/withMT";
 
-export default {
-  content: ["./src/**/*.{html,js}"],
+export default withMT({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+});
