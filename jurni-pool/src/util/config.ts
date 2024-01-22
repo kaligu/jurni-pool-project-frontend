f1e9f358a2a55f0755bc4ca632ead414f1859f88
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2024-01-22 15:25:09 +0530

    implimenting

--- jurni-pool/src/util/config.ts
+++ jurni-pool/src/util/config.ts
@@ -1,4 +1,4 @@
 export const GOOGLE_AUTH_CLIENT_ID = '439833973834-e1inhrr6q80nvv8kmtr0i3m9lpbh54nn.apps.googleusercontent.com';
 
 export const URL_SERVERAPI_USER_LOGIN = 'https://jurni-pool-project-backend.vercel.app/api/user/login';
-export const LOADTIMECALL_SERVERAPI_USER_LOGIN = 6;
+export const LOADTIMECALL_SERVERAPI_USER_LOGIN = 2;
