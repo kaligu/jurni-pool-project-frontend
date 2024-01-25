4bc5b7a0b809742610c59d00b4fad04daa8f577f
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2024-01-26 00:43:42 +0530

    implimenting account

--- jurni-pool/src/view/account/Account.tsx
+++ jurni-pool/src/view/account/Account.tsx
@@ -4,7 +4,16 @@ function Account() {
   return (
     <div>
       <NavigationBar clickedState={5}/>
-      <h1 className='text-white'>accounccct</h1>
+      
+      <div className='w-screen h-96 flex justify-center items-center'>
+        <div className='w-11/12 h-5/6 bg-[#F1FCFD] border-black border-2 rounded-lg'>
+          <div className='flex flex-col'>
+            <div className='ml-3 mt-3'><h1 className='text-lg font-semibold italic'> මාගේ වාහන </h1></div>
+
+            
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
