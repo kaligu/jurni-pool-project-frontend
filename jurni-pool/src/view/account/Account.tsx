8e27d6b67c1800a1eee99bb917da29bca47abfc0
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2024-01-26 09:06:41 +0530

    implimenting add form ui

--- jurni-pool/src/view/account/Account.tsx
+++ jurni-pool/src/view/account/Account.tsx
@@ -1,11 +1,11 @@
 import NavigationBar from '../../components/NavigationBar'
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
-import { red } from '@mui/material/colors';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import { useState } from 'react';
+import TextField from '@mui/material/TextField';
 
 const style = {
   position: 'absolute' as 'absolute',
@@ -28,36 +28,50 @@ function Account() {
 
   return (
     <div>
-      <NavigationBar clickedState={5}/>
-
-      
-      
-      <div className='w-screen h-96 flex justify-center items-center'>
-        <div className='w-11/12 h-5/6 bg-[#F1FCFD] border-black border-2 rounded-lg'>
-          <div className='flex flex-col'>
-            <div className='ml-3 mt-3'><h1 className='text-lg font-semibold italic'> මාගේ වාහන </h1></div>
-            <div className='ml-5 mt-3'><Button  variant="outlined" color="primary" startIcon={<AddIcon/>} onClick={handleOpen}>අලුත් වාහනයක් එක් කිරීමට</Button></div>
-            <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-    </div>
+    <NavigationBar clickedState={5} />
+  
+    <div className='w-screen h-96 flex justify-center items-center'>
+      <div className='w-11/12 h-5/6 bg-[#F1FCFD] border-black border-2 rounded-lg'>
+        <div className='flex flex-col'>
+          <div className='ml-3 mt-3'>
+            <h1 className='text-lg font-semibold italic'> මාගේ වාහන </h1>
+          </div>
+          <div className='ml-5 mt-3'>
+            <Button variant="outlined" color="primary" startIcon={<AddIcon />} onClick={handleOpen}>
+              අලුත් වාහනයක් එක් කිරීමට
+            </Button>
+          </div>
+          <div>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                ඔබගේ වාහනයේ විස්තර පහතින් පුරවන්න
+                </Typography>
+                <Typography id="modal-modal-content" sx={{ mt: 2 }}>
+                  *must
+                </Typography>
+                <div className='mt-5 ml-5 flex flex-col space-y-2'>
+                  <div>
+                  <TextField id="outlined-basic" label="වාහනයේ අංකය" variant="outlined" />
+                  </div>
+                  <div>
+                  <TextField id="outlined-basic" label="වාහනයේ අංකය" variant="outlined" />
+                  </div>
+                 
+                </div>
+              </Box>
+            </Modal>
           </div>
         </div>
       </div>
     </div>
+  </div>
+  
   )
 }
 
